8cbd45d014d25a460c11cf239069f15b257d8a67
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-11-19 21:41:22 -0500

    agregar ruta por id de producto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,45 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [producList, setProducList] = useState([
-    { name: "", price: "", image: "" },
+    { name: "", price: "", image: "",id :'' },
   ]);
+
+  const[loading, setLoading] = useState(true)
 
   useEffect(() => {
     window
       .fetch("/api/avo")
       .then((response) => response.json())
       .then(({ data, legth }) => {
-        setProducList(data);
+        setProducList(data);setLoading(false)
       });
   }, []);
 
   return (
-    <>
+    <>{loading? 'espere':
       <div className="contenedor-cards">
         {producList.map((avo) => (
+          <Link href={`product/${avo.id}`}>
           <div className="card-avo">
             <div>
-              <Image
-                src={avo.image}
-                alt={`logo ${avo.name}`}
-                width={300}
-                height={300}
-              ></Image>
+              <div className="image-card-avo">
+                <Image
+                  src={avo.image}
+                  alt={`logo ${avo.name}`}
+                  width={300}
+                  height={300}
+                ></Image>
+              </div>
             </div>
-            <div>{avo.name}</div>
-            <div>{avo.price}</div>
-          </div>
+            <div className="avo-name">{avo.name}</div>
+            <div className="avo-price">{avo.price}</div>
+          </div></Link>
         ))}
       </div>
-    </>
+    }</>
   );
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import data from "../../database/data";
+import Image from "next/image";
 
 const ProductItem = () => {
   const {
     query: { id },
   } = useRouter();
 
-  const [product, setProduct] = useState<TProduct>();
+  const [product, setProduct] = useState(
+    { name: "", price: "", image: "",id :'',attributes: {description:""},
+    },
+  );
 
   useEffect(() => {
     window
@@ -18,7 +22,12 @@ const ProductItem = () => {
       });
   }, [id]);
   console.log(data);
-  return <div>{product?.name}</div>;
+  return(<div><div>{product?.name}</div> <Image
+  src={product.image}
+  alt={`logo ${product?.name}`}
+  width={300}
+  height={300}
+></Image><div>{product.attributes.description}</div></div>) ;
 };
 
 export default ProductItem;
