51ad78c18b6afe75c236bab6099f5522b01dc859
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-11-20 13:21:59 -0500

    avanzar en página de detalle de producto

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,44 +2,59 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
 
 export default function Home() {
   const [producList, setProducList] = useState([
-    { name: "", price: "", image: "",id :'' },
+    { name: "", price: "", image: "", id: 0 },
   ]);
 
-  const[loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     window
       .fetch("/api/avo")
       .then((response) => response.json())
       .then(({ data, legth }) => {
-        setProducList(data);setLoading(false)
+        setProducList(data);
+        setLoading(false);
       });
   }, []);
 
   return (
-    <>{loading? 'espere':
-      <div className="contenedor-cards">
-        {producList.map((avo) => (
-          <Link href={`product/${avo.id}`}>
-          <div className="card-avo">
-            <div>
-              <div className="image-card-avo">
-                <Image
-                  src={avo.image}
-                  alt={`logo ${avo.name}`}
-                  width={300}
-                  height={300}
-                ></Image>
+    <>
+      {loading ? (
+        <div className="container-loading">
+          {" "}
+          <ReactLoading
+            type={"bubbles"}
+            color={"rgb(19, 88, 10)"}
+            height={600}
+            width={130}
+          />
+        </div>
+      ) : (
+        <div className="contenedor-cards">
+          {producList.map((avo) => (
+            <Link key={avo.id} href={`product/${avo.id}`}>
+              <div className="card-avo">
+                <div>
+                  <div className="image-card-avo">
+                    <Image
+                      src={avo.image}
+                      alt={`logo ${avo.name}`}
+                      width={300}
+                      height={300}
+                    ></Image>
+                  </div>
+                </div>
+                <div className="avo-name">{avo.name}</div>
+                <div className="avo-price">{avo.price}</div>
               </div>
-            </div>
-            <div className="avo-name">{avo.name}</div>
-            <div className="avo-price">{avo.price}</div>
-          </div></Link>
-        ))}
-      </div>
-    }</>
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -2,32 +2,88 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import data from "../../database/data";
 import Image from "next/image";
+import ReactLoading from "react-loading";
 
 const ProductItem = () => {
   const {
     query: { id },
   } = useRouter();
 
-  const [product, setProduct] = useState(
-    { name: "", price: "", image: "",id :'',attributes: {description:""},
-    },
-  );
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    sku: "",
+    image: "",
+    id: "",
+    attributes: { description: "", taste: "", hardiness: "", shape: "" },
+  });
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     window
-      .fetch(`/api/avo/${ id }`)
+      .fetch(`/api/avo/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setProduct(data);
+        setLoading(false);
       });
   }, [id]);
   console.log(data);
-  return(<div><div>{product?.name}</div> <Image
-  src={product.image}
-  alt={`logo ${product?.name}`}
-  width={300}
-  height={300}
-></Image><div>{product.attributes.description}</div></div>) ;
+  return (
+    <>
+      {loading ? (
+        <div className="container-loading">
+          {" "}
+          <ReactLoading
+            type={"bubbles"}
+            color={"rgb(19, 88, 10)"}
+            height={600}
+            width={130}
+          />
+        </div>
+      ) : (
+        <div className="contenedor-producto">
+          <div className="contenedor-hijo-producto">
+            <div className="contenedor-imagen-titulo">
+              <Image
+                src={product?.image}
+                alt={`logo ${product?.name}`}
+                width={300}
+                height={300}
+              ></Image>
+              <div>
+                <div>{product?.name}</div>
+                <div>$ {product?.price}</div>
+                <div>{product?.sku}</div>
+              </div>
+            </div>
+            <div>
+              <h3> About this avocado</h3>
+              <div>{product?.attributes.description}</div>
+            </div>
+            <div>
+              <table>
+                <th>Attributes</th>
+                <tr>
+                  <td>Taste</td>
+                  <td>{product?.attributes.taste}</td>
+                </tr>
+                <tr>
+                  <td>Hardiness</td>
+                  <td>{product?.attributes.hardiness}</td>
+                </tr>
+                <tr>
+                  <td>Shape</td>
+                  <td>{product?.attributes.shape}</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProductItem;
