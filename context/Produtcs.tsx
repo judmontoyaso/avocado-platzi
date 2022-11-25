61732255449f9198952b564bf80192d7993cb26a
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-11-24 19:34:06 -0500

    deploiment corregido

--- context/Produtcs.tsx
+++ context/Produtcs.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 
 
-export const ProductContext = createContext([{...{
+export const ProductContext = createContext<any[]>([{...{
  product:{ name: "",
  price: 0,
  sku: "",
